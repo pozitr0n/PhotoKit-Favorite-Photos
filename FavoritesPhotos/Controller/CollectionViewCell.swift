40fda3b4dfe20aa202ccd6fc3f5c60388b3989ee
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-04-27 02:33:33 +0200

    Adding files of the project into remote repository

---
+++ FavoritesPhotos/Controller/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  FavoritesPhotos
+//
+//  Created by Raman Kozar on 27/04/2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var favImageView: UIImageView!
+    
+}
---
+++ FavoritesPhotos/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FavoritesPhotos
+//
+//  Created by Raman Kozar on 27/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
